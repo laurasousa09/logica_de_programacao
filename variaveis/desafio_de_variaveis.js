5e623123fff4167059736ac5059488bf22d104e9
laura <lapisousa51@gmail.com>
2025-05-29 11:29:41 -0300

    variaveis

---
+++ variaveis/desafio_de_variaveis.js
@@ -0,0 +1,12 @@
+Desafio 1: Cadastro de Usuário
+let nome = prompt(&quot;Digite seu nome:&quot;)
+let idade = Number(prompt(&quot;Digite a sua idade: &quot;))
+let email = prompt(&quot;Digite seu email: &quot;)
+let telefone = Number(prompt(&quot;Digite seu número de telefone: &quot;))
+console.log(&quot;O seu nome é&quot;, nome, &quot;você tem&quot;,idade, &quot;anos, o seu email é&quot;, email, &quot;e
+o seu número de telefone é&quot;, telefone, &quot;correto? Seja bem vindo ao nosso site!&quot;)
+
+Desafio 2: Descobrindo a Idade
+let anoDeNascimento = Number(prompt(&quot;Digite seu ano de nascimento: &quot;))
+let subtração = 2025 - anoDeNascimento
+console.log (&quot;A sua idade é:&quot;, subtração)
---
+++ variaveis/variaveis.js
@@ -0,0 +1,41 @@
+1. Declarando uma variável e exibindo no console
+let nome = &quot;laura&quot;
+console.log (nome)
+2. Declarando um número e exibindo no console
+let idade = &quot;15&quot;
+console.log (idade)
+3. Criando uma variável booleana
+let aprendendoJavaScript = &quot;true&quot;
+console.log (aprendendoJavaScript)
+4. Concatenando strings
+let nome = &quot;laura&quot;
+let sobrenome = &quot;sousa&quot;
+let nomeCompleto = &quot;laura sousa&quot;
+console.log (nomeCompleto)
+console.log (sobrenome)
+console.log (nome)
+5. Somando dois números
+let numero1 = 10
+let numero2 = 25
+let soma = numero1 + numero2
+console.log (soma)
+6. Multiplicando dois números
+let numero1 = 10
+let numero2 = 25
+let multiplicação = numero1 * numero2
+console.log (multiplicação)
+7. Variável de texto e número
+let cidade = &quot;Mogi das Cruzes &quot;
+let habitantes = &quot;451.505&quot;
+console.log (cidade + habitantes)
+
+8. Alterando o valor de uma variável
+let preco = 20
+preco += 25
+console.log (preco)
+9. Criando uma variável sem valor (undefined)
+let meuValor = &quot;undefined&quot;
+console.log (meuValor)
+10. Criando um valor
+let meuNulo = &quot;null&quot;
+console.log (meuNulo)
